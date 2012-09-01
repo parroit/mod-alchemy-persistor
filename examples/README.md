a3fd368dfeb08990a19381774d692863b4537d42
Andrea Parodi <andrea.parodi@ebansoftware.net>
2012-09-01 16:40:28 +0200

    changed readme

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+#Alchemy Persistor
+
+This folder contains examples of use of the modules
+
+*Sorry, any example yet*
--- test/src/java/org/vertx/java/framework/testng/EventFields.java
+++ test/src/java/org/vertx/java/framework/testng/EventFields.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2012 Andrea Parodi.
+ * Copyright 2012 Andrea Parodi.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
